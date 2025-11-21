606b419ac094085b8e66e87140470f2c62b111d1
Hailu <hailegebrelyalemberabate@outlook.com>
2025-11-21 14:14:36 +0300

    done

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER, // From .env.production
-  host: process.env.DB_HOST, // Render host
-  database: process.env.DB_NAME, // Render database name
-  password: process.env.DB_PASS, // Render password
-  port: process.env.DB_PORT, // Usually 5432
-  ssl:
-    process.env.DB_SSL === "true" // Use SSL for Render in production
-      ? { rejectUnauthorized: false }
-      : false,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: Number(process.env.DB_PORT) || 5432,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
