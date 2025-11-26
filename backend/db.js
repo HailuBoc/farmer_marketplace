17642e571481eaa67c8044f0e06effc9b80b5c14
Hailu <hailegebrelyalemberabate@outlook.com>
2025-11-26 20:54:56 +0300

    absolutely working code

--- backend/db.js
+++ backend/db.js
@@ -1,24 +1,26 @@
 // db.js
 import pkg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config(); // Loads .env or .env.production automatically
-
 const { Pool } = pkg;
-
-const sslSetting =
-  process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false;
+import dotenv from "dotenv";
+dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
+  user: process.env.DB_USER || "farmers_marketplace_database_user",
+  host: process.env.DB_HOST || "dpg-d4i817n5r7bs73ca8k20-a",
+  database: process.env.DB_NAME || "farmers_marketplace_database",
+  password: process.env.DB_PASS || "EYuqv5QFXJ1zM0WK1ElrvGsad4fy7XQK",
   port: Number(process.env.DB_PORT) || 5432,
-  ssl: sslSetting,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
-
-// Test connection
+console.log(process.env.DB_USER);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_DB_NAME);
+console.log(process.env.DB_PASS);
+console.log(process.env.DB_PORT);
+// Optional: test the connection when the server starts
 pool
   .connect()
   .then(() => console.log("✅ Connected to PostgreSQL"))
