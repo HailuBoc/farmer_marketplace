58f00d3e9e716ef090cde7872355bc315bf48bd4
Hailu <hailegebrelyalemberabate@outlook.com>
2025-11-26 11:37:22 +0300

    new update

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "farmers_marketplace",
-  password: process.env.DB_PASS || "Hailkasa#35",
+  user: process.env.DB_USER || "farmers_marketplace_database_user",
+  host: process.env.DB_HOST || "dpg-d4i817n5r7bs73ca8k20-a",
+  database: process.env.DB_NAME || "farmers_marketplace_database",
+  password: process.env.DB_PASS || "EYuqv5QFXJ1zM0WK1ElrvGsad4fy7XQK",
   port: Number(process.env.DB_PORT) || 5432,
   ssl:
     process.env.NODE_ENV === "production"
