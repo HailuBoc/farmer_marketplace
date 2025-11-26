dca38f4b474cc196df0c85ec2439673ce8b53f1a
Hailu <hailegebrelyalemberabate@outlook.com>
2025-11-26 20:47:42 +0300

    locally and product level working app

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,24 @@
 // db.js
 import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config(); // Loads .env or .env.production automatically
+
 const { Pool } = pkg;
 
+const sslSetting =
+  process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false;
+
 export const pool = new Pool({
-  user: process.env.DB_USER || "farmers_marketplace_database_user",
-  host: process.env.DB_HOST || "dpg-d4i817n5r7bs73ca8k20-a",
-  database: process.env.DB_NAME || "farmers_marketplace_database",
-  password: process.env.DB_PASS || "EYuqv5QFXJ1zM0WK1ElrvGsad4fy7XQK",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
   port: Number(process.env.DB_PORT) || 5432,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: sslSetting,
 });
 
-// Optional: test the connection when the server starts
+// Test connection
 pool
   .connect()
   .then(() => console.log("✅ Connected to PostgreSQL"))
